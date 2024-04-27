22d276b5d3f132a8f2a869a981ec420f0a5998a4
hugendobler <luisach2003@gmail.com>
2024-04-27 15:45:00 -0300

    movimentos players

--- pong.py
+++ pong.py
@@ -11,16 +11,14 @@ class Ball:
     
 
 class Player:
-    def __init__(self, surf, color, x, y, width, height):
+    def __init__(self, surf, color, width, height):
         self.surf = surf
         self.color = color
-        self.x = x
-        self.y = y
         self.width = width
         self.height = height
 
-    def surface(self):
-        pygame.draw.rect(self.surf, self.color, (self.x, self.y, self.width, self.height))
+    def surface(self, x, y):
+        pygame.draw.rect(self.surf, self.color, (x, y, self.width, self.height))
 
 pygame.init()
 screen_width = 800
@@ -47,8 +45,8 @@ y_player1 = 2*screen_height/5
 x_player2 = screen_width-width_players
 y_player2 = 2*screen_height/5
 
-player1 = Player(screen, (255,255,255),x_player1, y_player1, width_players, height_players)
-player2 = Player(screen, (255,255,255), x_player2, y_player2, width_players, height_players)
+player1 = Player(screen, (255,255,255), width_players, height_players)
+player2 = Player(screen, (255,255,255), width_players, height_players)
 
 
 while running:
@@ -58,6 +56,24 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.KEYDOWN:
+            #player 1
+            if event.key == pygame.K_w:
+                if y_player1 >= 10: 
+                    y_player1 -=10
+            elif event.key == pygame.K_s:
+                if y_player1 < screen_height-height_players:
+                    y_player1 +=10
+            #player 2
+            if event.key == pygame.K_UP:
+                if y_player2 >= 10: 
+                    y_player2 -=10
+            elif event.key == pygame.K_DOWN:
+                if y_player2 < screen_height-height_players:
+                    y_player2 +=10
+
+        
     
     x += vel_x
     y += vel_y
@@ -66,8 +82,8 @@ while running:
     if y <= 0 or y >= screen_height: vel_y = - vel_y
 
     ball.surface(x, y)
-    player1.surface()
-    player2.surface()
+    player1.surface(x_player1, y_player1)
+    player2.surface(x_player2, y_player2)
 
     pygame.display.flip()
     pygame.time.delay(10)
