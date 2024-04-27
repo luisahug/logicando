1503493e354238844b74759eb3fed0e9fcb5a880
hugendobler <luisach2003@gmail.com>
2024-04-27 17:17:36 -0300

    texto placar

--- pong.py
+++ pong.py
@@ -48,39 +48,57 @@ y_player2 = 2*screen_height/5
 player1 = Player(screen, (255,255,255), width_players, height_players)
 player2 = Player(screen, (255,255,255), width_players, height_players)
 
+points_player1 = 0
+points_player2 = 0
+
+#informações placar de pontos
+font = pygame.font.Font(None, 36)
+text_score = font.render(" x " , True, (255,255,255))
+rect_score = text_score.get_rect()
+rect_score.center = (screen_width // 2, screen_height // 2)
+rect_score.y = 35
 
 while running:
     screen.fill((0,0,0))
-    
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-        if event.type == pygame.KEYDOWN:
-            #player 1
-            if event.key == pygame.K_w and y_player1 >= 20: 
-                y_player1 -=20
-            elif event.key == pygame.K_s and y_player1 < screen_height-height_players:
-                y_player1 +=20
-            #player 2
-            if event.key == pygame.K_UP and y_player2 >= 20: 
-                y_player2 -=20
-            elif event.key == pygame.K_DOWN and y_player2 < screen_height-height_players:
-                y_player2 +=20
-
-        
-    
+    #movimento dos players
+    keys = pygame.key.get_pressed()
+    #player 1
+    if keys[pygame.K_w] and y_player1 >= 5: 
+        y_player1 -=5
+    elif keys[pygame.K_s] and y_player1 < screen_height-height_players:
+        y_player1 +=5
+    #player 2
+    if keys[pygame.K_UP] and y_player2 >= 5: 
+        y_player2 -=5
+    elif keys[pygame.K_DOWN] and y_player2 < screen_height-height_players:
+        y_player2 +=5
+    #iteração da bola
     x += vel_x
     y += vel_y
+    
+    """colisões bola X players
+    rect_player1 = pygame.Rect(x_player1, y_player2, player1.width, player1.height)
+    rect_player2 = pygame.Rect(x_player2, y_player2, player2.width, player2.height)
+    rect_ball = pygame.Rect(x, y, ball.radius, ball.radius)
+    if rect_ball.collidepoint(x_player1, y_player1):
+        points_player1 += 1
+        vel_x = -vel_x
+    if rect_ball.collidepoint(x_player2, y_player2):
+        points_player2 += 1
+        vel_x = -vel_x
+    if x <= 0 or x >= screen_width: 
 
-    if x <= 0 or x >= screen_width: vel_x = - vel_x
-    if y <= 0 or y >= screen_height: vel_y = - vel_y
-
+    if y <= 0 or y >= screen_height: 
+"""
+    #exibição dos elementos
     ball.surface(x, y)
     player1.surface(x_player1, y_player1)
     player2.surface(x_player2, y_player2)
-
+    screen.blit(text_score, rect_score)
+    #atualização da tela a cada loop
     pygame.display.flip()
     pygame.time.delay(10)
 
